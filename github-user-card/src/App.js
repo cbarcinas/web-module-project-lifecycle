3ac111fdf12d62bc4e0f71fd8d2c4c2d19aa259c
Craig Barcinas <cbarcinas@live.com>
2021-07-11 19:49:14 -0700

    MVP

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -4,6 +4,7 @@ import Card from "./Card";
 import FollowerCard from "./FollowerCard";
 
 export default class App extends React.Component {
+  
   constructor() {
     super();
     this.state = {
@@ -13,29 +14,33 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    axios
-      .get("https://api.github.com/users/cbarcinas")
-      .then((res) => {
+    axios.get("https://api.github.com/users/cbarcinas").then(
+      (res) => {
         this.setState({ ...this.state, users: res.data });
-      })
-      .catch((err) => {
+      },
+      (err) => {
         console.log(err);
-      });
+      }
+    );
 
-    axios
-      .get("https://api.github.com/users/cbarcinas/followers")
-      .then((res) => {
+    axios.get("https://api.github.com/users/cbarcinas/followers").then(
+      (res) => {
         this.setState({ ...this.state, followers: res.data });
-      })
-      .catch((err) => {
+        
+      },
+      (err) => {
         console.log(err);
-      });
+      }
+    );
   }
 
   render() {
     return (
       <div>
         <Card users={this.state.users} />
+        {/* checking state to see if followers exist and if 
+        there is data to be mapped through. If both are true
+        return a Follower Card component with given props passed as params */}
         {this.state.followers &&
           this.state.followers.map((follower) => {
             return <FollowerCard key={follower.id} follower={follower} />;
